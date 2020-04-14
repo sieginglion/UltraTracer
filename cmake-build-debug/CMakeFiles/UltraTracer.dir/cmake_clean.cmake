b63bd791a7c25fc05f5e5a793fe5bfcfb53c21de
Jonathan Hsu <jon@Jonathans-Air.Home>
2020-04-14 11:29:11 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/UltraTracer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UltraTracer.dir/main.cpp.o"
+  "UltraTracer"
+  "UltraTracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UltraTracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
