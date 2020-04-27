0b38290cdaf0c8399cc75874bec70f6ccfae4c6f
sieginglion <Rick8morty>
2020-04-27 08:58:38 +0800

    reduce implicit type casting

--- main.cpp
+++ main.cpp
@@ -22,8 +22,8 @@ float Dot(__m128& a, __m128& b) {
 
 __m128 Cross(__m128& a, __m128& b) {
     __m128 _ = _mm_sub_ps(
-            _mm_mul_ps(a, _mm_shuffle_ps(b, b, _MM_SHUFFLE(3, 0, 2, 1))),
-            _mm_mul_ps(_mm_shuffle_ps(a, a, _MM_SHUFFLE(3, 0, 2, 1)), b)
+        _mm_mul_ps(a, _mm_shuffle_ps(b, b, _MM_SHUFFLE(3, 0, 2, 1))),
+        _mm_mul_ps(_mm_shuffle_ps(a, a, _MM_SHUFFLE(3, 0, 2, 1)), b)
     );
     return _mm_shuffle_ps(_, _, _MM_SHUFFLE(3, 0, 2, 1));
 }
@@ -73,18 +73,18 @@ float GetIntersect(array<__m128, 3>& V, Ray& R) {
     __m128 E2 = V[2] - V[0];
     __m128 P = Cross(R.D, E2);
     float d = Dot(E1, P);
-    if (d > -0.000001 && d < 0.000001) {
-        return 0;
+    if (d > -0.000001f && d < 0.000001f) {
+        return 0.0f;
     }
     __m128 T = R.O - V[0];
     float u = Dot(P, T) / d;
-    if (u < 0 || u > 1) {
-        return 0;
+    if (u < 0.0f || u > 1.0f) {
+        return 0.0f;
     }
     __m128 Q = Cross(T, E1);
     float v = Dot(R.D, Q) / d;
-    if (v < 0 || u + v > 1) {
-        return 0;
+    if (v < 0.0f || u + v > 1.0f) {
+        return 0.0f;
     }
     return Dot(E2, Q) / d;
 }
@@ -93,7 +93,7 @@ __m128 GetReflect(array<__m128, 3>& V, __m128& D) {
     __m128 E1 = V[1] - V[0];
     __m128 E2 = V[2] - V[0];
     __m128 N = Norm(Cross(E1, E2));
-    return D - N * Dot(D, N) * 2;
+    return D - N * Dot(D, N) * 2.0f;
 }
 
 float TraceRay(int max_iters, vector<array<__m128, 3>>& scene, Ray& ray, float reflectance, __m128& sun) {
@@ -127,10 +127,10 @@ void SaveScreen(int width, int height, float screen[]) {
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             if (j == width - 1) {
-                file << int(screen[width * i + j] * 255) << '\n';
+                file << static_cast<int>(screen[width * i + j] * 255.0f) << '\n';
             }
             else {
-                file << int(screen[width * i + j] * 255) << ' ';
+                file << static_cast<int>(screen[width * i + j] * 255.0f) << ' ';
             }
         }
     }
@@ -141,13 +141,14 @@ struct Timer {
     Timer() {
         T0 = std::chrono::steady_clock::now();
     }
-    ~Timer() {
+    void stop() {
         T1 = std::chrono::steady_clock::now();
         std::cout << (T1 - T0).count() / 1000000 << "ms\n";
     }
 };
 
 int main() {
+//    vector<string> FILENAMES = { "../ground.obj", "../teapot.obj", "../tetrahedron.obj" };
     vector<string> FILENAMES = { "ground.obj", "teapot.obj", "tetrahedron.obj" };
     float FOV = 60 * 0.0174; // field of view
     int WIDTH = 640;
@@ -162,14 +163,15 @@ int main() {
     float pixel = tan(FOV / 2) * 2 / WIDTH;
     __m128 dx = Norm(__m128{ CAM_DIR[1], -CAM_DIR[0], 0 }) * pixel;
     __m128 dy = { 0, 0, -pixel };
-    CAM_DIR = CAM_DIR - dx * (float(WIDTH) / 2) - dy * (float(HEIGHT) / 2);
+    CAM_DIR = CAM_DIR - dx * (WIDTH / 2.0f) - dy * (HEIGHT / 2.0f);
     float screen[WIDTH * HEIGHT];
     Timer timer;
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            Ray ray = { CAM_POS, Norm(CAM_DIR + dy * float(i) + dx * float(j)) };
+            Ray ray = { CAM_POS, Norm(CAM_DIR + dy * static_cast<float>(i) + dx * static_cast<float>(j)) };
             screen[WIDTH * i + j] = TraceRay(MAX_ITERS, scene, ray, REFLECTANCE, SUN);
         }
     }
+    timer.stop();
     SaveScreen(WIDTH, HEIGHT, screen);
 }
